fdb1dbaf0cb4fad6bb39662edab4b4abda5c6bfa
Aman Gaharwar <amangaharwar1103@gmail.com>
2022-08-22 13:35:21 +0530

    Added download button which downloads hash

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,80 +1,115 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
-import axios from 'axios';
-import './Details.css';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useState, useEffect } from "react";
+import html2canvas from "html2canvas";
+import jspdf from "jspdf";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./Details.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 export default function Details() {
-	let baseUrl;
-	if (process.env.NODE_ENV === 'development') {
-		baseUrl = process.env.REACT_APP_DEV_URL;
-	} else {
-		baseUrl = process.env.REACT_APP_PROD_URL;
-	}
-	const [certificates, setCertificates] = useState([]);
-	useEffect(() => {
-		const getCertificatesData = async () => {
-			const {data} = await axios.get(`${baseUrl}/certificates/all`);
-			setCertificates(data);
-		};
-		getCertificatesData();
-	}, [baseUrl]);
-	return (
-		<div className='container'>
-			<div className='header'>
-				<span>Timestamping</span>
-				<nav>
-					<Link to='/'>Home</Link>
-					<Link to='/details'>Details</Link>
-				</nav>
-			</div>
-			<div className='main'>
-				<TableContainer component={Paper}>
-					<Table sx={{minWidth: 650}} aria-label='simple table'>
-						<TableHead>
-							<TableRow>
-								<TableCell align='center'>
-									<strong>UUID</strong>
-								</TableCell>
-								<TableCell align='center'>
-									<strong>Hash</strong>
-								</TableCell>
-								<TableCell align='center'>
-									<strong>Tx Initiated</strong>
-								</TableCell>
-								<TableCell align='center'>
-									<strong>Tx Recorded</strong>
-								</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{certificates &&
-								certificates.map((row) => (
-									<TableRow
-										key={row._id}
-										sx={{
-											'&:last-child td, &:last-child th': {border: 0},
-										}}
-									>
-										<TableCell align='center'>{row.uuid}</TableCell>
-										<TableCell align='center'>{row.txHash}</TableCell>
-										<TableCell align='center'>
-											{row.txInitiatedTimestamp}
-										</TableCell>
-										<TableCell align='center'>
-											{row.txWrittenInBlockTimestamp}
-										</TableCell>
-									</TableRow>
-								))}
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</div>
-		</div>
-	);
+  let baseUrl;
+  if (process.env.NODE_ENV === "development") {
+    //baseUrl = process.env.REACT_APP_DEV_URL;
+    baseUrl = "http://localhost:4200/api";
+  } else {
+    //baseUrl = process.env.REACT_APP_PROD_URL;
+    baseUrl = "http://localhost:4200/api";
+  }
+  const [certificates, setCertificates] = useState([]);
+  useEffect(() => {
+    const getCertificatesData = async () => {
+      const { data } = await axios.get(`${baseUrl}/certificates/all`);
+      setCertificates(data);
+    };
+    getCertificatesData();
+  }, [baseUrl]);
+  const handleCertificateDownload = () => {
+    const cert = document.getElementById("maybeHidden");
+    console.log(cert);
+    console.log("he");
+    if (cert) {
+      cert.style.visibility = "visible";
+      html2canvas(cert).then((canvas) => {
+        const imgdata = canvas.toDataURL("images/png");
+        const pdf = new jspdf();
+        pdf.addImage(imgdata, "JPEG", 0, 0);
+        pdf.save("download.pdf");
+      });
+      cert.style.visibility = "hidden";
+    }
+  };
+  return (
+    <div className="container">
+      <div className="header">
+        <span>Timestamping</span>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/details">Details</Link>
+        </nav>
+      </div>
+      <div className="main">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">
+                  <strong>UUID</strong>
+                </TableCell>
+                <TableCell align="center">
+                  <strong>Hash</strong>
+                </TableCell>
+                <TableCell align="center">
+                  <strong>Tx Initiated</strong>
+                </TableCell>
+                <TableCell align="center">
+                  <strong>Tx Recorded</strong>
+                </TableCell>
+                <TableCell align="center">
+                  <strong>Download</strong>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {certificates &&
+                certificates.map((row) => (
+                  <TableRow
+                    key={row._id}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    {/*Certificate Creation*/}
+                    <div
+                      id="maybeHidden"
+                      style={{ visibility: "hidden", color: "red" }}
+                    >
+                      <h1>Timestamp Certificate</h1>
+                      <h3>{row.txHash}</h3>
+                    </div>
+                    <TableCell align="center">{row.uuid}</TableCell>
+                    <TableCell align="center">{row.txHash}</TableCell>
+                    <TableCell align="center">
+                      {row.txInitiatedTimestamp}
+                    </TableCell>
+                    <TableCell align="center">
+                      {row.txWrittenInBlockTimestamp}
+                    </TableCell>
+                    <TableCell>
+                      <button onClick={handleCertificateDownload}>
+                        Download
+                      </button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,163 +1,170 @@
-import React, {useRef, useEffect, useState} from 'react';
+import React, { useRef, useEffect, useState } from "react";
 
-import Button from '@mui/material/Button';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import axios from 'axios';
-import './Home.css';
-import {Alert, Snackbar} from '@mui/material';
-import {Link} from 'react-router-dom';
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
+import "./Home.css";
+import { Alert, Snackbar } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Home(props) {
-	let baseUrl;
-	if (process.env.NODE_ENV === 'development') {
-		baseUrl = process.env.REACT_APP_DEV_URL;
-	} else {
-		baseUrl = process.env.REACT_APP_PROD_URL;
-	}
-	const [allData, setAllData] = useState();
-	const [uploadedFile, setUploadedFile] = useState();
-	const [open, setOpen] = useState(false);
-	const [snackbarMessage, setSnackbarMessage] = useState();
-	const [snackbarColor, setSnackbarColor] = useState('success');
-	const handleClose = (event, reason) => {
-		if (reason === 'clickaway') {
-			return;
-		}
+  let baseUrl;
+  if (process.env.NODE_ENV === "development") {
+    //baseUrl = process.env.REACT_APP_DEV_URL;
+    baseUrl = "http://localhost:4200/api";
+  } else {
+    //baseUrl = process.env.REACT_APP_PROD_URL;
+    baseUrl = "http://localhost:4200/api";
+  }
+  const [allData, setAllData] = useState();
+  const [uploadedFile, setUploadedFile] = useState();
+  const [open, setOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState();
+  const [snackbarColor, setSnackbarColor] = useState("success");
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-		setOpen(false);
-	};
-	useEffect(() => {
-		async function getFiles() {
-			const {data} = await axios.get(`${baseUrl}/uploads`);
-			setAllData(data);
-		}
-		getFiles();
-	}, [baseUrl]);
-	const fileRef = useRef(null);
+    setOpen(false);
+  };
+  useEffect(() => {
+    async function getFiles() {
+      console.log(baseUrl);
+      const { data } = await axios.get(`${baseUrl}/uploads`);
+      setAllData(data);
+    }
+    getFiles();
+  }, [baseUrl]);
+  const fileRef = useRef(null);
 
-	const handleClick = (event) => {
-		fileRef.current.click();
-	};
-	const handleChange = (event) => {
-		const fileUploaded = event.target.files[0];
-		setUploadedFile(fileUploaded);
-	};
+  const handleClick = (event) => {
+    fileRef.current.click();
+  };
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0];
+    setUploadedFile(fileUploaded);
+  };
 
-	const uploadFile = async () => {
-		let formData = new FormData();
-		formData.append('file', uploadedFile);
-		try {
-			const resp = await axios.post(`${baseUrl}/upload`, formData);
-			setSnackbarMessage(resp.message);
-			setSnackbarColor('success');
-		} catch (err) {
-			setSnackbarMessage(err.response.data.message);
-			setSnackbarColor('error');
-			setOpen(true);
-		}
-	};
-	const generateCertificate = async (hash) => {
-		try {
-			const resp = await axios.post(`${baseUrl}/certificate/new`, {
-				hash: hash._id,
-			});
-			setSnackbarMessage(resp.message);
-			setSnackbarColor('success');
-		} catch (err) {
-			setSnackbarMessage(err.response.data.message);
-			setSnackbarColor('error');
-			setOpen(true);
-		}
-	};
+  const uploadFile = async () => {
+    let formData = new FormData();
+    formData.append("file", uploadedFile);
+    try {
+      const resp = await axios.post(`${baseUrl}/upload`, formData);
+      setSnackbarMessage(resp.message);
+      setSnackbarColor("success");
+    } catch (err) {
+      setSnackbarMessage(err.response.data.message);
+      setSnackbarColor("error");
+      setOpen(true);
+    }
+  };
+  const generateCertificate = async (hash) => {
+    try {
+      const resp = await axios.post(`${baseUrl}/certificate/new`, {
+        hash: hash._id,
+      });
+      setSnackbarMessage(resp.message);
+      setSnackbarColor("success");
+    } catch (err) {
+      setSnackbarMessage(err.response.data.message);
+      setSnackbarColor("error");
+      setOpen(true);
+    }
+  };
 
-	return (
-		<div className='container'>
-			<div className='header'>
-				<span>Timestamping</span>
-				<nav>
-					<Link to='/'>Home</Link>
-					<Link to='/details'>Details</Link>
-				</nav>
-			</div>
-			<div className='left'>
-				<input
-					type='file'
-					ref={fileRef}
-					onChange={handleChange}
-					style={{display: 'none'}}
-				/>
-				<Button variant='contained' onClick={handleClick}>
-					Upload
-				</Button>
-				{uploadedFile && (
-					<div>
-						<span>
-							<b>FileName:</b> {uploadedFile.name}
-						</span>
-						<br />
-						<span>
-							<b>File Size:</b> {uploadedFile.size}
-						</span>
-						<Button type='outline' onClick={uploadFile}>
-							Submit
-						</Button>
-					</div>
-				)}
-			</div>
-			<div className='right'>
-				<TableContainer component={Paper}>
-					<Table sx={{minWidth: 650}} aria-label='simple table'>
-						<TableHead>
-							<TableRow>
-								<TableCell align='center'>
-									<strong>File Name</strong>
-								</TableCell>
-								<TableCell align='center'>
-									<strong>Original Name</strong>
-								</TableCell>
-								<TableCell align='center'>
-									<strong>Size</strong>
-								</TableCell>
-								<TableCell align='center'>
-									<strong>Action</strong>
-								</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{allData &&
-								allData.map((row) => (
-									<TableRow
-										key={row._id}
-										sx={{
-											'&:last-child td, &:last-child th': {border: 0},
-										}}
-									>
-										<TableCell align='center'>{row.fileName}</TableCell>
-										<TableCell align='center'>{row.originalName}</TableCell>
-										<TableCell align='center'>{row.size}</TableCell>
-										<TableCell align='center'>
-											<Button onClick={() => generateCertificate(row)}>
-												Generate
-											</Button>
-										</TableCell>
-									</TableRow>
-								))}
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</div>
-			<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-				<Alert onClose={handleClose} severity={snackbarColor} sx={{width: '100%'}}>
-					{snackbarMessage}
-				</Alert>
-			</Snackbar>
-			;
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="header">
+        <span>Timestamping</span>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/details">Details</Link>
+        </nav>
+      </div>
+      <div className="left">
+        <input
+          type="file"
+          ref={fileRef}
+          onChange={handleChange}
+          style={{ display: "none" }}
+        />
+        <Button variant="contained" onClick={handleClick}>
+          Upload
+        </Button>
+        {uploadedFile && (
+          <div>
+            <span>
+              <b>FileName:</b> {uploadedFile.name}
+            </span>
+            <br />
+            <span>
+              <b>File Size:</b> {uploadedFile.size}
+            </span>
+            <Button type="outline" onClick={uploadFile}>
+              Submit
+            </Button>
+          </div>
+        )}
+      </div>
+      <div className="right">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">
+                  <strong>File Name</strong>
+                </TableCell>
+                <TableCell align="center">
+                  <strong>Original Name</strong>
+                </TableCell>
+                <TableCell align="center">
+                  <strong>Size</strong>
+                </TableCell>
+                <TableCell align="center">
+                  <strong>Action</strong>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {allData &&
+                allData.map((row) => (
+                  <TableRow
+                    key={row._id}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell align="center">{row.fileName}</TableCell>
+                    <TableCell align="center">{row.originalName}</TableCell>
+                    <TableCell align="center">{row.size}</TableCell>
+                    <TableCell align="center">
+                      <Button onClick={() => generateCertificate(row)}>
+                        Generate
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity={snackbarColor}
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+      ;
+    </div>
+  );
 }
